8b053784da3f6ea3446ac43f9d55429c7c4b05cc
Yar N <74919751+yaroslavopsguru@users.noreply.github.com>
2023-05-18 16:17:18 -0700

    Update main.tf
    Signed-off-by: Yar N. <74919751+yaroslavopsguru@users.noreply.github.com>

--- terraform-gcp/cluster-1/main.tf
+++ terraform-gcp/cluster-1/main.tf
@@ -1,6 +1,6 @@
 module "vault" {
   source                                       = "terraform-google-modules/vault/google"
-  version                                      = "6.1.1"
+  version                                      = "7.0.0"
   project_id                                   = var.project_id
   allow_public_egress                          = var.allow_public_egress
   allow_ssh                                    = var.allow_ssh
